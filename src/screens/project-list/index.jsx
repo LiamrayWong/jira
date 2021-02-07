c80e60eae10a36bf3507d78b0325007761c48b2e
LiamrayWong <7939060+LiamrayWong@user.noreply.gitee.com>
2021-02-07 20:59:46 +0800

    feat: 添加 useMount 和 useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import { SearchPanel } from "./SearchPanel";
 import { List } from "./List";
 import { cleanObject } from "../../utils";
+import {useMount,useDebounce} from "../../utils";
+
 import qs from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL;
+
 
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
@@ -14,26 +17,48 @@ export const ProjectListScreen = () => {
   //从managers中找到personId，读取db.json中的name属性
   const [managers, setManagers] = useState([]);
   const [list, setList] = useState([]);
+  const debouncedParam = useDebounce(param, 2000);
+
 
   //param变化时，页面请求项目列表的接口
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
       if (response.ok) {
         //保存项目列表的数据
         setList(await response.json());
       }
     });
-  }, [param]);
-  useEffect(() => {
+  }, [debouncedParam]);
+
+  //初始化负责人列表
+  useMount(() => {
     fetch(`${apiUrl}/managers`).then(async response => {
       if (response.ok) {
-        //保存项目列表的数据
+        //保存负责人列表的数据
         setManagers(await response.json());
       }
     });
-  }, []);
+  });
+
+
   return <div>
     <SearchPanel managers={managers} param={param} setParam={setParam} />
     <List managers={managers} list={list} />
   </div>;
 };
+
+
+// export const useDebounce = (value, delay) => {
+//     let timerId = null;
+//     let result = {...value}
+//     return ()=>{
+//       if(timerId){
+//         clearTimeout(timerId)
+//         result = null
+//       }
+//       timerId = setTimeout(()=>{
+//         return result
+//       },delay)
+//     }
+// };
+
--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => value === 0 ? false : !value;
 
 
@@ -12,3 +14,23 @@ export const cleanObject = (object) => {
   });
   return result;
 };
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, [callback]);
+};
+
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  //每次value变化执行一次
+  useEffect(() => {
+    //每次在value变化以后，设置一个定时器
+    const timerId = setTimeout(() => setDebouncedValue(value), value);
+    //清除上一次的timerId
+    return () => clearTimeout(timerId);
+  }, [value, delay]);
+  return debouncedValue;
+};
+
