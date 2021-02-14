7880e31de5a0c2a9c3e51aedae24c8c1a3ef13fe
LiamrayWong <7939060+LiamrayWong@user.noreply.gitee.com>
2021-02-14 21:13:36 +0800

    chore: 安装 devtool

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { LoginScreen } from "./screens/login";
 // import { ProjectListScreen } from "./screens/project-list";
+
+
 function App() {
   return (
     <div className="App">
