51916ff09d1531a39228305304b0a488bdfbc505
LiamrayWong <7939060+LiamrayWong@user.noreply.gitee.com>
2021-02-18 15:16:19 +0800

    style: 用 Raw 布局页面

--- src/authenticatedApp.tsx
+++ src/authenticatedApp.tsx
@@ -1,14 +1,40 @@
 import React from "react";
 import { ProjectListScreen } from "./screens/projectList";
 import { useAuth } from "./context/authContext";
-import { LongButton } from "./unauthenticatedApp";
+import styled from "@emotion/styled";
+import { Raw } from "./components/lib";
 
 export const AuthenticatedApp = () => {
-  const {logout} = useAuth()
+  const { logout } = useAuth();
   return (
     <div>
-      <LongButton onClick={logout}>登出</LongButton>
-      <ProjectListScreen />
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
     </div>
   );
 };
+
+
+const Header = styled(Raw)`
+  height: 6rem;
+`;
+
+
+const HeaderLeft = styled(Raw)``;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  height: calc(100vh - 6rem);
+`;
